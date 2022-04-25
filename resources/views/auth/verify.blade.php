5e898fb9a23b4438e0479c1a975c934ad2e2ab3d
ovi <mohammadahmadidv@gmail.com>
2022-04-25 08:03:39 -0700

    Uplod project on Site

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -4,22 +4,30 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
-
-                <div class="card-body">
+            <div class="card mt-5 bg-light text-warning">
+                <div class="card-header bg-dark">{{ __('تایید ایمیل') }}</div>
+                <div class="card-body bg-dark">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            {{ __('لینک فعال سازی مجدد ارسال شد!') }}
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
+                    {{ __('قبل از درخواست دوباره  لطفا ایمیل خود را چک کنید !') }}<br>
+                    {{ __('اگر ایمیلی دریافت نکردید') }},
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('مجدد کلیک کنید') }}</button>.
                     </form>
+                </div>
+                <div class="card-body bg-light  ">
+                    <div class="text-center ">
+                        <video width="320" height="240" autoplay loop muted>
+                            <source src="/main/assets/img/logo/logo.mp4" type="video/mp4">
+                            <source src="/main/assets/img/logo/logo.mp4" type="video/ogg">
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'RadioNamana') }}</title>
+    <title>RadioNamana</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
